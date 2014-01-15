498217b32f11760afe065ad74d2cd2969245685d
Marcus Broberg <broberg.marcus@gmail.com>
2014-01-15 09:43:08 +0100

    remove try-except and inserted eventhandler

--- lcd_terminal/lcd_terminal.py
+++ lcd_terminal/lcd_terminal.py
@@ -6,37 +6,42 @@ import subprocess
 import code
 import sys
 import time
+import signal
+
+def signal_handler(signal, frame):
+    print "Exiting the script"
+    lcd.clear()
+    sys.exit(0)
+
+signal.signal(signal.SIGINT, signal_handler)
 
 #Initiate the LCD
 lcd = Adafruit_CharLCDPlate()
 lcd.clear()
 
-output=subprocess.Popen(["ls"], stdout=subprocess.PIPE).communicate()[0]
+output=subprocess.Popen(["ifconfig"], stdout=subprocess.PIPE).communicate()[0]
 output=output.split("\n")
 start=0
-try:
-    while True:
-        if lcd.buttonPressed(lcd.DOWN):
-            while lcd.buttonPressed(lcd.DOWN):
-                pass
-            lcd.clear()
-            lcd.message("%s\n%s"%(output[start],output[start+1]))
-            while not(lcd.buttonPressed(lcd.DOWN)  or lcd.buttonPressed(lcd.UP)):
-                time.sleep(0.5)
-                lcd.scrollDisplayLeft()
-            if (start+1)<len(output):
-                start+=1
-        if lcd.buttonPressed(lcd.UP):
-            while lcd.buttonPressed(lcd.UP):
-                pass
-            lcd.clear()
-            lcd.message("%s\n%s"%(output[start],output[start+1]))
-            while not(lcd.buttonPressed(lcd.DOWN) or lcd.buttonPressed(lcd.UP)):
-                time.sleep(0.5)
-                lcd.scrollDisplayLeft()
-            if (start-1)>0:
-                start-=1
-                      
-except KeyboardInterrupt: #The program will run until ctrl-c is pressed
-    lcd.clear()
+while True:
+    if lcd.buttonPressed(lcd.DOWN):
+        while lcd.buttonPressed(lcd.DOWN):
+            pass
+        lcd.clear()
+        lcd.message("%s\n%s"%(output[start],output[start+1]))
+        while not(lcd.buttonPressed(lcd.DOWN)  or lcd.buttonPressed(lcd.UP)):
+            time.sleep(0.5)
+            lcd.scrollDisplayLeft()
+        if (start+1)<len(output):
+            start+=1
+    if lcd.buttonPressed(lcd.UP):
+        while lcd.buttonPressed(lcd.UP):
+            pass
+        lcd.clear()
+        lcd.message("%s\n%s"%(output[start],output[start+1]))
+        while not(lcd.buttonPressed(lcd.DOWN) or lcd.buttonPressed(lcd.UP)):
+            time.sleep(0.5)
+            lcd.scrollDisplayLeft()
+        if (start-1)>0:
+            start-=1
+            
         
